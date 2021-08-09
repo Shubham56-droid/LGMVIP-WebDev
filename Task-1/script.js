39869e837dd2d96e3922669279553891013bfec0
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-08-09 22:12:59 +0530

    Add files via upload

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,1 +1,14 @@
-// javascript file
+// image  slider  script
+
+var counter = 1;
+setInterval(function(){
+
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+
+    if(counter > 5)
+    {
+        counter = 1;
+    }
+
+},5000);
