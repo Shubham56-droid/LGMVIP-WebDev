4784b7783f84839e8dfd658e0d0020e3a6249483
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-08-12 22:30:36 +0530

    Add files via upload

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,14 +1,39 @@
 // image  slider  script
 
 var counter = 1;
-setInterval(function(){
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
 
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
+  if (counter > 5) {
+    counter = 1;
+  }
+}, 5000);
 
-    if(counter > 5)
-    {
-        counter = 1;
-    }
+let imgbase = document.querySelector(".img-base");
+let videoView = document.querySelector(".video-view");
+const videoproject = document.querySelector(".video-project");
 
-},5000);
+
+videoproject.addEventListener("click", (e) => {
+  if (e.target.classList.contains("img-base")) {
+   document.getElementById("onckc").click(); 
+  }
+});
+
+
+
+
+
+
+document.querySelectorAll(".video-project video").forEach((vid)=>{
+
+  vid.onclick =() =>{
+    videoView.classList.add("active");
+    document.querySelector('.video-view video').src = vid.getAttribute(src);
+  }
+});
+
+function popupproject() {
+  videoView.classList.remove("active");
+}
