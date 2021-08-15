ec60f1e294b623eb2ce3ae8c15a31130aeadcc2d
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-08-15 22:45:56 +0530

    Add files via upload

--- Task-1/script.js
+++ Task-1/script.js
@@ -31,5 +31,57 @@ function popupproject() {
 }
 
 
-// jquery owl crousel
 
+var primarySlider = new Splide( '#primary-slider', {
+	type       : 'fade',
+	heightRatio: 0.5,
+	pagination : false,
+	arrows     : false,
+	cover      : true,
+} );
+
+var secondarySlider = new Splide( '#secondary-slider' ).mount();
+var primarySlider = new Splide( '#primary-slider' );
+primaryslider.sync( secondarySlider ).mount();
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	var secondarySlider = new Splide( '#secondary-slider', {
+		fixedWidth  : 100,
+		height      : 60,
+		gap         : 10,
+		cover       : true,
+		isNavigation: true,
+		focus       : 'center',
+		breakpoints : {
+			'600': {
+				fixedWidth: 66,
+				height    : 40,
+			}
+		},
+	} ).mount();
+	
+	var primarySlider = new Splide( '#primary-slider', {
+		type       : 'fade',
+		heightRatio: 0.5,
+		pagination : false,
+		arrows     : false,
+		cover      : true,
+	} ); // do not call mount() here.
+	
+	primarySlider.sync( secondarySlider ).mount();
+} );
+
+var secondarySlider = new Splide( '#secondary-slider', {
+	fixedWidth : 100,
+	height     : 60,
+	gap        : 10,
+	rewind     : true,
+	cover      : true,
+	pagination : false,
+	breakpoints : {
+		'600': {
+			fixedWidth: 66,
+			height    : 40,
+		}
+	}
+} ).mount();
