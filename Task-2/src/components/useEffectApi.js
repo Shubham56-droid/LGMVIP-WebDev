1f5fc6dc16fe7bde13a416d4ac79503c6c7bfabe
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-08-20 23:10:13 +0530

    Add files via upload

---
+++ Task-2/src/components/useEffectApi.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const UseEffectApi = () => {
+ 
+  return (
+    <div className="container">
+      <p className="heading">Lists Of Users</p>
+      <div className="users">
+
+
+        <div className="user-item">
+          <div className="avator">
+            <img src="" alt="" />
+          </div>
+          <div className="info">
+            <p>Id Number: 1</p>
+            <br />
+            <span>First Name: Shubham</span>
+            <br />
+            <span>Last Name: Bawankar</span>
+            <br />
+            <span>Email: shubhambawankar@gmail.com</span>
+          </div>
+        </div>
+
+       
+      
+
+        
+      </div>
+    </div>
+  );
+};
