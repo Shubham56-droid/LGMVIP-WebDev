c832f4018d9811d3b54d86621415e3d073d7ab57
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-08-20 23:08:37 +0530

    Add files via upload

--- Task-2/src/App.js
+++ Task-2/src/App.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
+import {UseEffectApi} from "./components/useEffectApi";
 
+const App=()=>{
+
+ const [users, setUsers] = useState('Hello');
+ const loadUsers = async() =>{
+   const response = await fetch ("https://reqres.in/api/users?page=1");
+   const jsonresponse = await response.json;
+   setUsers( jsonresponse);
+
+ }
+
+  return (
+    <div className="App">
+       <button>Get Users</button>
+       <h2>Users:</h2>
+       <ul>
+        {users.map(({id,login}) =>
+        (
+          <li key={id}>Name:{login}</li>
+        ))}
+       </ul>
+    </div>
+  )
+}
+
+export default App;
